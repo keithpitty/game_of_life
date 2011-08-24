41e55565742c0472c0289c12044e92e4b1d7677c
Keith Pitty <keith@keithpitty.com>
2011-08-24 21:25:31 +1000

    Refactored

--- conway.rb
+++ conway.rb
@@ -2,19 +2,75 @@ class Conway
   
   attr_accessor :state
   
-  def cell_at(x, y)
+  def alive?(x, y)
     @state[x % @state.size][y % @state[0].size]
   end
   
+  def dead?(x, y)
+    !alive?(x, y)
+  end
+  
+  
   def next_state
-    result = new_blank_state
-    (0...@state.size).each do |x|
-      (0...@state[0].size).each do |y|
-        result[x][y] = apply_rule_1(x, y) && apply_rule_3(x,y)
+    result = alive_after_rule? do |x,y|
+      #alive_after_rule_1?(x, y) && alive_after_rule_2?(x,y) && alive_after_rule_3?(x,y) && alive_after_rule_4?(x,y)
+      count = number_of_living_neighbours(x,y)
+      alive = alive?(x,y)
+      dead = !alive
+      if alive && count < 2
+        false
+      elsif alive && [2,3].include?(count)
+        true
+      elsif alive && count > 3
+        false
+      elsif dead && count == 3
+        true
+      else
+        false
       end
     end
     result
   end
+  
+  def advance_state
+    @state = next_state
+  end
+  
+  def alive_after_rule?
+    raise "Fuck!" unless block_given?
+    result = new_blank_state
+    (0...@state.size).each do |x|
+      (0...@state[0].size).each do |y|
+        result[x][y] = yield(x,y)
+      end
+    end
+    result
+  end
+  
+  def height
+    @state[0].size
+  end
+  
+  def width
+    @state.size
+  end
+  
+  def to_s
+    result = ""
+    (0...height).each do |y|
+      (0...width).each do |x|
+        if alive?(x,y)
+          result << '*'
+        else
+          result << ' '
+        end
+      end
+      result << "\n"
+    end
+    result
+  end
+  
+  private
   
   def new_blank_state
     result = []
@@ -27,24 +83,53 @@ class Conway
     result
   end
   
-  def surrounding_cells(x, y)
-    [ cell_at(x-1, y-1),
-      cell_at(x, y-1),
-      cell_at(x+1, y-1),
-      cell_at(x-1, y),
-      cell_at(x+1, y),
-      cell_at(x-1, y+1),
-      cell_at(x, y+1),
-      cell_at(x+1, y+1)
+  def neighbours(x, y)
+    [ alive?(x-1, y-1),
+      alive?(x, y-1),
+      alive?(x+1, y-1),
+      alive?(x-1, y),
+      alive?(x+1, y),
+      alive?(x-1, y+1),
+      alive?(x, y+1),
+      alive?(x+1, y+1)
     ]
   end
   
-  def apply_rule_1(x, y)
-    cell_at(x,y) && surrounding_cells(x, y).select{|c| c}.size >= 2
+  def alive_after_rule_1?(x, y)
+    alive?(x,y) && number_of_living_neighbours(x, y) >= 2
+  end
+
+  def alive_after_rule_2?(x, y)
+    alive?(x,y) && [2,3].include?(number_of_living_neighbours(x, y))
   end
   
-  def apply_rule_3(x, y)
-    cell_at(x,y) && !(surrounding_cells(x, y).select{|c| c}.size > 3)
+  def alive_after_rule_3?(x, y)
+    alive?(x,y) && number_of_living_neighbours(x, y) <= 3
   end
   
+  def alive_after_rule_4?(x,y)
+    dead?(x,y) && number_of_living_neighbours(x, y) == 3
+  end
+  
+  def number_of_living_neighbours(x, y)
+    neighbours(x, y).select{|c| c}.size
+  end
+  
+end
+
+if __FILE__ == $0
+  @game = Conway.new
+  initial_state = []
+  (0...70).each do |x|
+    initial_state << []
+    (0...20).each do |y|
+      initial_state[x] << (rand() < 0.1)
+    end
+  end
+  @game.state = initial_state
+  while true
+    puts @game
+    sleep 0.25
+    @game.advance_state
+  end
 end
--- conway_test.rb
+++ conway_test.rb
@@ -14,16 +14,16 @@ class ConwayTest < Test::Unit::TestCase
       [false, false, false]
     ]
     @game.state = initial_state
-    assert !@game.cell_at(0,0)
-    assert !@game.cell_at(0,1)
-    assert !@game.cell_at(0,2)
-    assert !@game.cell_at(1,0)
-    assert @game.cell_at(1,1)
-    assert !@game.cell_at(1,2)
-    assert !@game.cell_at(2,0)
-    assert !@game.cell_at(2,1)
-    assert !@game.cell_at(2,2)
-    assert @game.cell_at(4,4)
+    assert !@game.alive?(0,0)
+    assert !@game.alive?(0,1)
+    assert !@game.alive?(0,2)
+    assert !@game.alive?(1,0)
+    assert @game.alive?(1,1)
+    assert !@game.alive?(1,2)
+    assert !@game.alive?(2,0)
+    assert !@game.alive?(2,1)
+    assert !@game.alive?(2,2)
+    assert @game.alive?(4,4)
   end
   
   def test_rule_1
@@ -42,26 +42,58 @@ class ConwayTest < Test::Unit::TestCase
     assert_equal(expected_state, actual_state)
   end
 
-  def test_rule_3
+  def test_rule_2
     initial_state = [
       [false, false, false, false],
-      [false, true, false, false],
       [false, true, true, false],
-      [false, false, false, true]
+      [false, true, true, true],
+      [false, false, false, false]
     ]
     expected_state = [
       [false, false, false, false],
       [false, true, false, false],
-      [false, true, true, false],
+      [false, true, false, true],
       [false, false, false, false]
     ]
     @game.state = initial_state
-    actual_state = @game.next_state
+    actual_state = @game.alive_after_rule? {|x,y| @game.alive_after_rule_2?(x,y) }
+    assert_equal(expected_state, actual_state)
+  end
+  
+  def test_rule_3
+    initial_state = [
+      [false, false, false, false],
+      [false, true, true, false],
+      [false, true, true, true],
+      [false, false, false, false]
+    ]
+    expected_state = [
+      [false, false, false, false],
+      [false, true, false, false],
+      [false, true, false, true],
+      [false, false, false, false]
+    ]
+    @game.state = initial_state
+    actual_state = @game.alive_after_rule? {|x,y| @game.alive_after_rule_3?(x,y) }
     assert_equal(expected_state, actual_state)
   end
 
   def test_rule_4
-
+      initial_state = [
+        [false, false, false, false],
+        [false, false, false, false],
+        [false, true, true, true],
+        [false, false, false, false]
+      ]
+      expected_state = [
+         [false, false, false, false],
+          [false, false, true, false],
+          [false, false, false, false],
+          [false, false, true, false]
+      ]
+      @game.state = initial_state
+      actual_state = @game.alive_after_rule? {|x,y| @game.alive_after_rule_4?(x,y) }
+      assert_equal(expected_state, actual_state)
   end
   
 end
